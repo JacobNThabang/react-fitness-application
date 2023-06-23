3e2a95781f956ed2d6d1efc5a60ed80f1b60a505
Jacob Ntesang <ntesangjacob@gmail.com>
2023-06-23 11:59:14 +0200

    * Added the `Home` component as the root component to the App. * Added another componet `SideBar` and added * all files that were not commited yesterday.

--- src/components/ExerciseList.js
+++ src/components/ExerciseList.js
@@ -1,34 +1,40 @@
-const Exercises = [
+const ExerciseList = [
     {
         "id":1,
-        "name":"The Trophy",
-        "image":"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.muscleandfitness.com%2Froutine%2Fworkouts%2Fworkout-routines%2Ftrophy-muscle-workout-d-i%2F&psig=AOvVaw3OCGEqmFkihTrd13jycDn3&ust=1687374682307000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNCpodbG0v8CFQAAAAAdAAAAABAJ",
+        "name":"The Pushups",
+        "image":"https://images.everydayhealth.com/images/consisten-exercise-helps-anxiety-and-stress-1440x810.jpg?w=1110",
         "description": "The Trophy Muscle Workout: Pump up your legs, shoulders, and abs on day 1 - Muscle & Fitness"
     },
     {
         "id":2,
         "name":"Biceps Workouts",
-        "image":"Biceps Workouts: How to Correct the 5 Biggest Mistakes - The Barbell",
-        "description": "The Trophy Muscle Workout: Pump up your legs, shoulders, and abs on day 1 - Muscle & Fitness"
+        "image": "https://images.everydayhealth.com/images/consisten-exercise-helps-anxiety-and-stress-1440x810.jpg?w=1110",
+        "description": "Biceps Workouts: How to Correct the 5 Biggest Mistakes - The Barbell"
     },
     {
         "id": 3,
         "name":"The Trophy",
-        "image":"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.muscleandfitness.com%2Froutine%2Fworkouts%2Fworkout-routines%2Ftrophy-muscle-workout-d-i%2F&psig=AOvVaw3OCGEqmFkihTrd13jycDn3&ust=1687374682307000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNCpodbG0v8CFQAAAAAdAAAAABAJ",
+        "image":"https://images.everydayhealth.com/images/consisten-exercise-helps-anxiety-and-stress-1440x810.jpg?w=1110",
         "description": "The Trophy Muscle Workout: Pump up your legs, shoulders, and abs on day 1 - Muscle & Fitness"
     },
     {
         "id": 4,
-        "name":"The Trophy",
-        "image":"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.muscleandfitness.com%2Froutine%2Fworkouts%2Fworkout-routines%2Ftrophy-muscle-workout-d-i%2F&psig=AOvVaw3OCGEqmFkihTrd13jycDn3&ust=1687374682307000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNCpodbG0v8CFQAAAAAdAAAAABAJ",
+        "name":"Standing overhead dumbbell presses",
+        "image":"https://images.everydayhealth.com/images/consisten-exercise-helps-anxiety-and-stress-1440x810.jpg?w=1110",
         "description": "The Trophy Muscle Workout: Pump up your legs, shoulders, and abs on day 1 - Muscle & Fitness"
     },
     {
         "id": 5,
         "name":"The Trophy",
-        "image":"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.muscleandfitness.com%2Froutine%2Fworkouts%2Fworkout-routines%2Ftrophy-muscle-workout-d-i%2F&psig=AOvVaw3OCGEqmFkihTrd13jycDn3&ust=1687374682307000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNCpodbG0v8CFQAAAAAdAAAAABAJ",
+        "image":"https://images.everydayhealth.com/images/consisten-exercise-helps-anxiety-and-stress-1440x810.jpg?w=1110",
+        "description": "The Trophy Muscle Workout: Pump up your legs, shoulders, and abs on day 1 - Muscle & Fitness"
+    },
+    {
+        "id": 6,
+        "name":"The Trophy",
+        "image":"https://images.everydayhealth.com/images/consisten-exercise-helps-anxiety-and-stress-1440x810.jpg?w=1110",
         "description": "The Trophy Muscle Workout: Pump up your legs, shoulders, and abs on day 1 - Muscle & Fitness"
     },
 ];
 
-export default Exercises;
+export default ExerciseList;
